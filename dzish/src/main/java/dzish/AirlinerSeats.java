4b5e695d3cab989f98d12f32ddd41c28c668fa31
jach <jach@192.168.0.90>
2016-01-22 15:12:23 +0100

    +: skelet

--- dzish/src/main/java/dzish/AirlinerSeats.java
+++ dzish/src/main/java/dzish/AirlinerSeats.java
@@ -1,6 +1,10 @@
 package dzish;
 
-
 public class AirlinerSeats {
+    
+    public String[] mostAisleSeats(int width, int seats) {
+    
+        return new String[0];
+    }
     
 }
--- dzish/src/test/java/dzish/AirlinerSeatsTest.java
+++ dzish/src/test/java/dzish/AirlinerSeatsTest.java
@@ -1,16 +1,108 @@
 package dzish;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
-
 
 public class AirlinerSeatsTest {
     
-    @Test
-    public void test() {
+    private AirlinerSeats airlinerSeats = new AirlinerSeats();
     
-        fail("Not yet implemented");
+    
+    @Test
+    public void test1() {
+    
+        // Assert
+        int width = 6;
+        int seats = 3;
+        
+        // Act
+        String[] seatsRow = airlinerSeats.mostAisleSeats(width, seats);
+        
+        // Assert
+        Assert.assertArrayEquals(new String[] { ".", ".", "S", "S", ".", "S" }, seatsRow);
+    }
+    
+    
+    @Test
+    public void test2() {
+    
+        // Assert
+        int width = 6;
+        int seats = 4;
+        
+        // Act
+        String[] seatsRow = airlinerSeats.mostAisleSeats(width, seats);
+        
+        // Assert
+        Assert.assertArrayEquals(new String[] { "S", ".", "S", "S", ".", "S" }, seatsRow);
+    }
+    
+    
+    @Test
+    public void test3() {
+    
+        // Assert
+        int width = 12;
+        int seats = 10;
+        
+        // Act
+        String[] seatsRow = airlinerSeats.mostAisleSeats(width, seats);
+        
+        // Assert
+        Assert.assertArrayEquals(new String[] { "S", ".", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S" }, seatsRow);
+    }
+    
+    
+    @Test
+    public void test4() {
+    
+        // Assert
+        int width = 11;
+        int seats = 7;
+        
+        // Act
+        String[] seatsRow = airlinerSeats.mostAisleSeats(width, seats);
+        
+        // Assert
+        Assert.assertArrayEquals(new String[] { ".", "S", "S", ".", "S", "S", ".", "S", "S", ".", "S" }, seatsRow);
+    }
+    
+    
+    @Test
+    public void test5() {
+    
+        // Assert
+        int width = 52;
+        int seats = 52;
+        String[] expected = new String[52];
+        for (int i = 0; i < width; i++) {
+            expected[i] = "S";
+        }
+        
+        // Act
+        String[] seatsRow = airlinerSeats.mostAisleSeats(width, seats);
+        
+        // Assert
+        Assert.assertArrayEquals(new String[] { "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S" }, seatsRow);
+    }
+    
+    
+    @Test
+    public void test6() {
+    
+        // Assert
+        int width = 52;
+        int seats = 52;
+        String[] expected = new String[52];
+        for (int i = 0; i < width; i++) {
+            expected[i] = "S";
+        }
+        
+        // Act
+        String[] seatsRow = airlinerSeats.mostAisleSeats(width, seats);
+        
+        // Assert
+        Assert.assertArrayEquals(new String[] { "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S" }, seatsRow);
     }
     
 }
