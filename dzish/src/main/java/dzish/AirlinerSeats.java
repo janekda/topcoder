6b39f43bbd8d7abedeed3f4ac8b8f6ec88afb975
jach <jach@192.168.0.90>
2016-01-22 14:54:38 +0100

    + new files

---
+++ dzish/src/main/java/dzish/AirlinerSeats.java
@@ -0,0 +1,6 @@
+package dzish;
+
+
+public class AirlinerSeats {
+    
+}
---
+++ dzish/src/test/java/dzish/AirlinerSeatsTest.java
@@ -0,0 +1,16 @@
+package dzish;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class AirlinerSeatsTest {
+    
+    @Test
+    public void test() {
+    
+        fail("Not yet implemented");
+    }
+    
+}
